b5d329c172fd2ba1f7f97b4164c850b0da88572f
Tobias Hersvik Ørnehaug <71593112+Tobias-Hersvik-Ornehaug@users.noreply.github.com>
2026-01-31 15:54:08 +0100

    hmm

--- scripts/knight.gd
+++ scripts/knight.gd
@@ -6,15 +6,26 @@ const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var double_jump_available = true
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		double_jump_available = true
+		
+	else:
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+		if Input.is_action_just_pressed("jump") and double_jump_available:
+			velocity.y = JUMP_VELOCITY
+			double_jump_available = false
+			
+			
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		double_jump_available = true
+		
 
 	
 	var direction := Input.get_axis("move_left", "move_right")
---
+++ scripts/transition.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var entered = false
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	entered = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	entered = false
+	
+
+func _process(delta: float) -> void:
+	if entered:
+		get_tree().change_scene_to_file("res://scenes/platformer_test_2.tscn")
