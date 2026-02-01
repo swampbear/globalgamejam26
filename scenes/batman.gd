c482a515439ebd2542766d5448a1c54aaedfb8a5
Kanasukr <calmel.david@gmail.com>
2026-02-01 14:52:50 +0100

    crowd cheers

--- scenes/batman.gd
+++ scenes/batman.gd
@@ -12,4 +12,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var audio_choice = str(randi_range(0,9))
+	var crowd_audio_src = "res://audio/Crowd/Crowd_Bonus_0"+audio_choice+".wav"
+	audio.stream = load(crowd_audio_src)
+	#audio.play()
 	pass
