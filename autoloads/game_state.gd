9002f4cf1993e24794847e1897af78e20c529da9
Kanasukr <calmel.david@gmail.com>
2026-02-01 12:00:07 +0100

    added hero one liner (dirty switch)

--- autoloads/game_state.gd
+++ autoloads/game_state.gd
@@ -3,6 +3,7 @@ extends Node
 var current_mask: MaskData = null
 var current_level: int = 0
 var current_level_path: String = ""
+var intro_hero_sound_src: String = ""
 
 var levels: Array = [
 	{"id":0, "name": "I", "path": "res://scenes/pick_up_test.tscn", "unlocked": true},
@@ -22,3 +23,16 @@ func get_unlocked_count() ->int:
 			total += 1
 		
 	return total
+
+func get_intro_hero_sound():
+	match current_mask.type:
+		MaskData.MaskType.THIEF:
+			intro_hero_sound_src = "res://audio/Thief/Thief_01.wav"
+		MaskData.MaskType.ANGEL:
+			intro_hero_sound_src = "res://audio/Angel/Angel_01.wav"
+		MaskData.MaskType.DEVIL:
+			intro_hero_sound_src = "res://audio/Devil/Devil_01.wav"
+		MaskData.MaskType.KNIGHT:
+			intro_hero_sound_src = "res://audio/Knight/Knight_01.wav"
+		MaskData.MaskType.CYCLOPS:
+			intro_hero_sound_src = "res://audio/Cyclops/Cyclops_01.wav"
---
+++ scenes/pick_up_test.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var audio = $audio
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GameState.get_intro_hero_sound()
+	audio.stream = load(GameState.intro_hero_sound_src)
+	audio.play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
