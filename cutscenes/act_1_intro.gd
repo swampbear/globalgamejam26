a2f8da7e4d1283c33c7c06dc2b11a4dfe1691795
Jobjorn <672641@stud.hvl.no>
2026-02-01 11:09:38 +0100

    add masks, ressize mask selection

--- cutscenes/act_1_intro.gd
+++ cutscenes/act_1_intro.gd
@@ -12,14 +12,14 @@ func _ready():
 	
 	# Animate
 	var tween = create_tween()
-	tween.tween_property(act_title, "modulate:a", 1.0, 0.8)
+	tween.tween_property(act_title, "modulate:a", 1.0, 0.8) 
 	tween.tween_property(subtitle, "modulate:a", 1.0, 0.5)
 	tween.tween_property(mask_image, "modulate:a", 1.0, 1.0)
 	tween.tween_interval(2.0)
 	tween.tween_callback(_next)
 
 func _next():
-	get_tree().change_scene_to_file("res://cutscenes/act1_monologue.tscn")
+	get_tree().change_scene_to_file("res://menus/maskmenu/mask_selection.tscn")
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
